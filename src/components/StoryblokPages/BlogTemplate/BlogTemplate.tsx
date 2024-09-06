c0256ea449d3ba62a769c5923487b61dac5a4fe7
Victor Adolfsson <victoreadolfsson@gmail.com>
2024-09-06 14:44:58 +0200

    feat: add styling to blogpage

--- src/components/StoryblokPages/BlogTemplate/BlogTemplate.tsx
+++ src/components/StoryblokPages/BlogTemplate/BlogTemplate.tsx
@@ -11,29 +11,26 @@ interface Props {
 const BlogTemplate: FC<Props> = ({ blok }) => {
   const { image, title, subtitle, date, author, body } = blok;
   return (
-    <main className={styles.contentWrapper} {...storyblokEditable(blok)}>
-      <Image
-        src={image.filename}
-        alt={image.alt}
-        width={1200}
-        height={600}
-        style={{ objectFit: 'cover' }}
-        className={styles.featuredImage}
-      />
-      <div className={styles.intro}>
-        <h1 className={styles.title}>{title}</h1>
-        <p className={styles.author}>Written by: {author}</p>
-        <p className={styles.date}>Published: {date}</p>
+    <div className={styles.container} {...storyblokEditable(blok)}>
+      <div className={styles.featuredImage}>
+        <Image src={image.filename} alt={image.alt} layout="fill" objectFit="cover" />
       </div>
-      <div className={styles.content}>
-        <h2 className={styles.subtitle}>{subtitle}</h2>
-        <div className={styles.richText}>
-          {body.map((nestedBlok: any) => (
-            <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-          ))}
+      <div className={styles.contentGrid}>
+        <div className={styles.intro}>
+          <h1 className={styles.title}>{title}</h1>
+          <p className={styles.author}>Written by: {author}</p>
+          <p className={styles.date}>Published: {date}</p>
+        </div>
+        <div className={styles.content}>
+          <h2 className={styles.subtitle}>{subtitle}</h2>
+          <div className={styles.body}>
+            {body.map((nestedBlok: any) => (
+              <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+            ))}
+          </div>
         </div>
       </div>
-    </main>
+    </div>
   );
 };
 
