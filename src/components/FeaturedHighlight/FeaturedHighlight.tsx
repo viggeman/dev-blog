c2f3f428492f981205d1d1d99e40944cdd15f26b
Victor Adolfsson <victoreadolfsson@gmail.com>
2024-09-27 13:10:11 +0200

    add blog highlights

---
+++ src/components/FeaturedHighlight/FeaturedHighlight.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import { FC } from 'react';
+import styles from './FeaturedHighlight.module.scss';
+
+interface Props {
+  highlight: any;
+}
+
+const FeaturedHighlight: FC<Props> = ({ highlight }) => {
+  console.log('highlight', highlight);
+  const { title, image, link } = highlight;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textWrapper}>
+        <h2>{title}</h2>
+      </div>
+      <div className={styles.imageWrapper}>
+        <Image src={image.filename} alt={image.alt} layout="fill" objectFit="cover" />
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedHighlight;
